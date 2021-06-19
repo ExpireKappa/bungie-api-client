74e7e307125e5927d8cad420cc1d6a0fb05a0771
Kieran <kieran.weston00@gmail.com>
2021-06-19 20:14:18 +0100

    Convert app to use react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import {PlayerSearch} from "./components/playerSearch/playerSearch.component";
+import {Route, BrowserRouter as Router, Switch} from "react-router-dom";
+
 import './App.css';
 
 function App() {
     return (
-      <div className="App">
-        <PlayerSearch />
-      </div>
+        <Router>
+            <Switch>
+                <Route path="/" exact>
+                    <PlayerSearch />
+                </Route>
+            </Switch>
+        </Router>
     );
 }
 
