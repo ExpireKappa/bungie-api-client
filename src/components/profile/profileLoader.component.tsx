2e906e1429257646d6c41b1ef395f1a6b1d1d12a
Kieran <kieran.weston00@gmail.com>
2021-06-22 22:12:26 +0100

    Get logging working for my bungie account - no validation VERY cursed

--- src/components/profile/profileLoader.component.tsx
+++ src/components/profile/profileLoader.component.tsx
@@ -1,5 +1,5 @@
 import React, {Component, ReactElement} from "react";
-import {getUserById} from "../../services/userRequestService";
+import {getProfile, getUserById, searchDestinyPlayer} from "../../services/userRequestService";
 import {IUserItem} from "../playerSearch/interfaces/IUserItem";
 import {Profile} from "./profile.component";
 
@@ -20,13 +20,30 @@ export class ProfileLoader extends Component<IProfileLoaderProps, IProfileLoader
 
     }
 
-    getDestinyProfile() {
+    getDestinyProfile(profile: IUserItem) {
+        let membershipType: number = 0;
+        if (profile?.steamDisplayName) {
+            membershipType = 3;
+        } else if (profile?.psnDisplayName) {
+            membershipType = 2;
+        } else if (profile?.xboxDisplayName) {
+            membershipType = 1;
+        }
 
+        if (profile?.displayName) {
+            searchDestinyPlayer(membershipType, profile.displayName).then((response) => {
+                console.log(response.Response[0].membershipId)
+                getProfile(membershipType, response.Response[0].membershipId).then((response) => {
+                    console.log(response)
+                })
+            })
+        }
     }
 
     getBungieProfile() {
         getUserById(this.props.membershipId).then((response) => {
             this.setState({bungieProfile: response.Response})
+            this.getDestinyProfile(response.Response)
         })
     }
 
--- src/services/userRequestService.ts
+++ src/services/userRequestService.ts
@@ -9,6 +9,10 @@ const platformRoot = "https://www.bungie.net/Platform";
 const User_SearchUsers = "/User/SearchUsers";
 const User_GetUserById = "/User/GetBungieNetUserById/{id}/";
 
+// Destiny2
+const Destiny2_GetProfile = "/Destiny2/{membershipType}/Profile/{destinyMembershipId}/";
+const Destiny2_SearchDestinyPlayer = "/Destiny2/SearchDestinyPlayer/{membershipType}/{displayName}/";
+
 export const searchUsers = (q: string) => {
     return new RequestService().get(`${platformRoot + User_SearchUsers}`, [["q", q]])
         .then((response: Response) => {
@@ -22,6 +26,28 @@ export const searchUsers = (q: string) => {
 
 export const getUserById = (id: string) => {
     return new RequestService().get(`${platformRoot + User_GetUserById.replace("{id}", id)}`, [])
+        .then((response: Response) => {
+            if (response.status !== 200) {
+                throw new Error("Error searching users");
+            }
+
+            return response.json();
+        })
+}
+
+export const getProfile = (membershipType: number, destinyMembershipId: string) => {
+    return new RequestService().get(`${platformRoot + Destiny2_GetProfile.replace("{membershipType}", membershipType.toString()).replace("{destinyMembershipId}", destinyMembershipId)}?components=200`, [])
+        .then((response: Response) => {
+            if (response.status !== 200) {
+                throw new Error("Error searching users");
+            }
+
+            return response.json();
+        })
+}
+
+export const searchDestinyPlayer = (membershipType: number, displayName: string) => {
+    return new RequestService().get(`${platformRoot + Destiny2_SearchDestinyPlayer.replace("{membershipType}", membershipType.toString()).replace("{displayName}", displayName)}`, [])
         .then((response: Response) => {
             if (response.status !== 200) {
                 throw new Error("Error searching users");
