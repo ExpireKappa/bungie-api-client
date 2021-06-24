8d0c01297a9cc3b80ccecdf48d941df3239f1450
Kieran <kieran.weston00@gmail.com>
2021-06-24 23:04:46 +0100

    Move http away from api definitions

--- src/services/api/requestBase.ts
+++ src/services/api/requestBase.ts
@@ -1,11 +1,22 @@
 import {config} from "../../dev.config";
+import {HttpClientConfig} from "bungie-api-ts/http";
 
 interface IRequestConfig {
     method: string;
     url: URL
 }
 
-// todo: still not happy with this, still causes duplication of code
+// todo: remove export once all methods are converted over to api helpers
 export const send = (requestConfig: IRequestConfig) => {
     return fetch(requestConfig.url.toString(), {method: requestConfig.method, headers: {"x-api-key": config.apiKey}});
 }
+
+export const http = (httpConfig: HttpClientConfig): Promise<any> => {
+    const url = new URL(httpConfig.url);
+    for (let key in httpConfig.params) {
+        url.searchParams.append(key, httpConfig.params[key]);
+    }
+
+    return send({url: url, method: httpConfig.method});
+}
+
