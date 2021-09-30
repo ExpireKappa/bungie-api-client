92daa6b7234ff7af0610b12356a26b111eb0048b
Kieran <kieran.weston00@gmail.com>
2021-09-30 21:20:40 +0100

    Create a generic input component
    - Overrides for bungie specific styles for now
    - No additional element functionality

---
+++ src/components/Input/Input.tsx
@@ -0,0 +1,11 @@
+import React, { FunctionComponent, FocusEvent, useState } from "react";
+
+import "./input.css";
+
+export const Input: FunctionComponent<React.HTMLProps<HTMLInputElement>> = (props) => {
+    return (
+        <div className={`generic-input__border`}>
+            <input {...props} className={`generic-input ${props.className}`}/>  
+        </div>
+    );
+}
