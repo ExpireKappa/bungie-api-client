6ab2cc87dd443858efd5023cf762625777ad12a6
Kieran <kieran.weston00@gmail.com>
2021-09-30 21:31:41 +0100

    Move playerCard info to an array instead of being static

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -3,32 +3,43 @@ import { Input } from "../../components/Input/Input";
 
 import "./home.css";
 
+interface IPlayerCard {
+    avatarPath: string;
+    name: string;
+}
+
 export const Home: FunctionComponent = () => {
+    const players: Array<IPlayerCard> = [
+        {
+            avatarPath: "/logo192.png",
+            name: "Reasonable Name"
+        },
+        {
+            avatarPath: "/logo192.png",
+            name: "Super Long Name That Could Be Really Painful"
+        },
+        {
+            avatarPath: "/logo192.png",
+            name: "Name"
+        }
+    ]
+    
     return (
         <div className={"search"}>
             <div className={"search__input-container"}>
                 <Input className={"search__input"} placeholder="Search by Bungie Name"/>
             </div>
             <div className={"search-results"}>
-                {/* Mock Data for now */}
-                <div className={"search-result"}>
-                    <img className={"search-result__player-avatar"} src="/logo192.png" alt="Player avatar" />
-                    <div className={"search-result__player-info"}>
-                        <span>Reasonable Name</span>
-                    </div>
-                </div>
-                <div className={"search-result"}>
-                    <img className={"search-result__player-avatar"} src="/logo192.png" alt="Player avatar" />
-                    <div className={"search-result__player-info"}>
-                        <span>Super Long Name That Could Be Really Painful</span>
-                    </div>
-                </div>
-                <div className={"search-result"}>
-                    <img className={"search-result__player-avatar"} src="/logo192.png" alt="Player avatar" />
-                    <div className={"search-result__player-info"}>
-                        <span>Name</span>
-                    </div>
-                </div>
+                {players.map(player => {
+                    return (
+                        <div className={"search-result"}>
+                            <img className={"search-result__player-avatar"} src={player.avatarPath} alt="Player avatar" />
+                            <div className={"search-result__player-info"}>
+                                <span>{player.name}</span>
+                            </div>
+                        </div>
+                    )
+                })}
             </div>
         </div>
     );
