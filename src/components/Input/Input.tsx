11944e9c0c17d4be03d2e6d74225ba37f1ac60d2
Kieran <kieran.weston00@gmail.com>
2021-10-01 22:30:10 +0100

    Add box-shadow to input focus
    Should make the user aware of the element selected since i've disabled
    outline. It was weird having it only on hover

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,11 +1,17 @@
-import React, { FunctionComponent } from "react";
+import React, { FunctionComponent, useState } from "react";
 
 import "./input.css";
 
 export const Input: FunctionComponent<React.HTMLProps<HTMLInputElement>> = (props) => {
+    let [isFocussed, setIsFocussed] = useState<boolean>(false);
+    
     return (
-        <div className={`generic-input__border`}>
-            <input {...props} className={`generic-input ${props.className}`}/>  
+        <div className={`generic-input__border ${isFocussed ? "focus" : ""}`}>
+            <input {...props} 
+                className={`generic-input ${props.className}`}
+                onFocus={() => setIsFocussed(true)}
+                onBlur={() => setIsFocussed(false)}/>  
         </div>
     );
 }
+
