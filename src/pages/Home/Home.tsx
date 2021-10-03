ca7c602507ba901e6a0f52beb01fbf4e0e1caad0
Kieran <kieran.weston00@gmail.com>
2021-10-03 11:21:51 +0100

    Remove non-destiny accounts from search list

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -12,35 +12,48 @@ interface IPlayerCard {
 
 export const Home: FunctionComponent = () => {
     let [players, setPlayers] = useState<Array<IPlayerCard>>([]);
+    let [inputValue, setInputValue] = useState<string>("");
 
     const onTextInputChange = (e: ChangeEvent<HTMLInputElement>) => {
-        const inputValue = e.currentTarget.value;
+        const value = e.target.value;
+        setInputValue(value);
 
-        if (inputValue === "") {
+        if (value === "") {
             setPlayers([]);
             return;
         }
 
-        SearchByGlobalNamePrefix(inputValue, 0).then(response => {
+        SearchByGlobalNamePrefix(value, 0).then(response => {
+            if (response.searchResults.length === 0) {
+                setPlayers([]);
+                return;
+            }
+            
+            const validUsers = response.searchResults.filter(player => player.destinyMemberships.length !== 0);
+            
             // Could do extra processing to split bungie name inputs by #
             // Then sort the results by the provided bungieId
-            const formattedUsers = response.searchResults.map(player => ({
-                platformIconPath: player.destinyMemberships[0].iconPath,
-                displayName: player.bungieGlobalDisplayName
-            }))
+            const formattedUsers = validUsers.map(player => {
+                return {
+                    displayName: player.bungieGlobalDisplayName,
+                    platformIconPath: player.destinyMemberships[0].iconPath
+                }
+            })
             
             // Feel the need for debouncing on the input here
             setPlayers(formattedUsers); 
         });
     }
 
+    const containsNoProfiles = players.length === 0 && inputValue !== "";
     return (
         <div className={"search"}>
             <div className={"search__input-container"}>
                 <Input className={"search__input"} placeholder="Search by Bungie Name" onChange={onTextInputChange}/>
             </div>
             <div className={"search-results"}>
-                {players && players.map(player => {
+                {containsNoProfiles && <span>No profiles found</span>}
+                {players.length > 0 && players.map(player => {
                     return (<DestinyProfileCard player={player} key={players.indexOf(player)}/> )
                 })}
             </div>
